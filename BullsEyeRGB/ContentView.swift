5a4cc9b99a0a70820e2381470fc93a6de207dbde
tunmice <t.otegbayo@gmail.com>
2021-08-24 01:32:28 +0100

    Initial Commit

---
+++ BullsEyeRGB/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BullsEyeRGB
+//
+//  Created by Tunmice on 24/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
